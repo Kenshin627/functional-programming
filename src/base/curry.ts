8ede5044e7b08ac9e35fcd19c78d1de3e18c7bd9
slayer_0 <87zhaoxiaobo@163.com>
2022-08-23 10:45:59 +0800

    curry

--- src/base/curry.ts
+++ src/base/curry.ts
@@ -4,7 +4,7 @@ const curry =(fn: (...args: any[]) => any) => {
     let func = (...args1: unknown[]) => {
         if (args1.length < fn.length) {
             return (...args2: unknown[]) => {
-                return func(...args1.concat(...args2));
+                return func(...args1.concat(args2));
             }
         }else { 
             return fn(...args1);
@@ -18,4 +18,41 @@ const curryAdd = curry(add);
 console.log(curryAdd(1,2,3));
 console.log(curryAdd(1,2)(3));
 console.log(curryAdd(1)(2)(3));
+
+let filter = curry((f: (item: unknown) => boolean,v: unknown[]) => {
+    return v.filter(f);
+})
+
+
+interface person {
+    name: string;
+    age: number;
+}
+
+let people: person[] = [
+    {
+        name: "zhangsan",
+        age: 18
+    },
+    {
+        name: "lisi",
+        age: 21
+    },
+    {
+        name: "wangwu",
+        age: 22
+    },
+    {
+        name: "zhaoliu",
+        age: 16
+    },
+    {
+        name: "c",
+        age: 30
+    },
+]
+let filterByAge = filter((v: person) => v.age > 20);
+let res = filterByAge(people).map((p:any) => p.name);
+console.log(res);
+
 export { curry }
